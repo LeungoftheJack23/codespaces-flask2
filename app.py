cb8d2314f932939a3ae28b32bfbcfc207ea53a4c
Leung Ho Yin <23065363d@connect.polyu.hk>
2025-01-21 06:01:02 +0000

    comment app.py

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+# an endpoint that returns a random number
+# @app.route("/random")
+# def random_number():
+#     import randomresponse = {
+#         'randomNumber': 
+#     }
